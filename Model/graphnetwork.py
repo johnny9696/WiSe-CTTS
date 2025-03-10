35bc7294ba5d1a3a4bb7f5bcfd2e3a939deb123b
Junbum Kim <johnny960423@gmail.com>
2025-03-10 16:53:54 +0900

    update

--- Model/graphnetwork.py
+++ Model/graphnetwork.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+from attention import MultiHeadAttention, BidirectionalAttention, PositionwiseFeedForward
 
 class Auxilary_Classifier(nn.Module):
     def __init__(self, input_dim, num_classes, dropout = 0.3, level = "local"):
@@ -100,28 +101,61 @@ class Audio_encoder(nn.Module):
 
         return output
 
-class TA_Encoder(nn.Module):
-    def __init__(self, preprocess_config, model_config, level = "local"):
-        super(TA_Encoder, self).__init__()
-        self.n_mels = preprocess_config["mel"]["n_mel_channels"]
+
+class Global_Feature_Extractor(nn.Module):
+    def __init__(self, preprocess_config, model_config):
+        super(Global_Feature_Extractor, self).__init__()
+        self.n_mels = preprocess_config["preprocessing"]["mel"]["n_mel_channels"]
         self.text_dim = model_config["WiSeGCN"]["TA_Encoder"]["text_dim"]
         self.hidden_dim = model_config["WiSeGCN"]["TA_Encoder"]["hidden"]
-        self.conv_kernel = model_config["WiSeGCN"]["TA_Encoder"]["conv_kernel"]
-        self.n_heads = model_config["WiSeGCN"]["TA_Encoder"]["n_heads"]
         self.n_layers = model_config["WiSeGCN"]["TA_Encoder"]["n_layers"]
-        self.n_speaker = model_config["WiSeGCN"]["Auxilary_Task"]["speakers"]
-        self.n_emotion = model_config["WiSeGCN"]["Auxilary_Task"]["emotion"]
-        self.n_act = model_config["WiSeGCN"]["Auxilary_Task"]["act"]
+        self.A_conv_kernel = model_config["WiSeGCN"]["TA_Encoder"]["Audio_Encoder"]["conv_kernel"]
+        self.A_n_layers = model_config["WiSeGCN"]["TA_Encoder"]["Audio_Encoder"]["n_layers"]
+        self.A_dropout = model_config["WiSeGCN"]["TA_Encoder"]["Audio_Encoder"]["dropout"]
+        self.n_speaker = model_config["WiSeGCN"]["TA_Encoder"]["Auxilary_Task"]["speakers"]
+        self.n_emotion = model_config["WiSeGCN"]["TA_Encoder"]["Auxilary_Task"]["emotion"]
+        self.n_act = model_config["WiSeGCN"]["TA_Encoder"]["Auxilary_Task"]["act"]
 
 
-        self.level = level
+        self.audio_encoder = Audio_encoder(self.n_mels, self.hidden_dim, self.A_conv_kernel, self.A_n_layers, self.A_dropout, level = "global")
+        self.audio_aux_speaker = Auxilary_Classifier(self.hidden_dim, self.n_speaker, level="global")
+        self.audio_aux_emotion = Auxilary_Classifier(self.hidden_dim, self.n_emotion, level="global")
+        self.audio_aux_act = Auxilary_Classifier(self.hidden_dim, self.n_act, level="global")
 
-        self.text_linear = nn.Linear(self.text_dim)
-        self.audio_encoder = Audio_encoder(self.n_mels, self.hidden_dim, self.conv_kernel,  self.n_layers, self.dropout ,level =level)
-        self.audio_aux_speaker= Auxilary_Classifier(self.hidden_dim, self.n_speaker, level = level)
-        self.audio_aux_emotion = Auxilary_Classifier(self.hidden_dim, self.n_emotion, level = level)
-        self.audio_aux_act = Auxilary_Classifier(self.hidden_dim, self.n_act, level = level)
+        self.text_linear = nn.Linear(self.text_dim, self.hidden_dim)
 
+        self.alpha_mlp = nn.Sequential(
+            nn.Linear(self.hidden_dim*2, self.hidden_dim),
+            nn.ReLU(),
+            nn.Linear(self.hidden_dim, 1),
+            nn.Sigmoid()
+        )
 
+        self.attention = MultiHeadAttention(self.hidden_dim, self.hidden_dim, self.hidden_dim, 1, self.hidden_dim)
 
-    def forward(self):
+    def forward(self, sbert, mel, history_mask, audio_mask):
+        """
+        :param sbert: b, h, c
+        :param mel: b, h, l, c
+        :param history_mask: b, h
+        :param audio_mask: b, h, l
+        :return:
+        """
+        b, h,_ = sbert.size()
+        _,_,l,c =mel.size()
+        sbert = sbert.contiguous().view(b * h, -1)
+        mel = mel.contiguous().view(b*h, l, c)
+        history_mask = history_mask.unsqueeze(-1).contiguous().view(b*h,1).expand(-1,self.hidden_dim)
+        audio_mask = audio_mask.contiguous().view(b*h, l)
+
+        text = self.text_linear(sbert).masked_fill(history_mask, 0)
+        audio = self.audio_encoder(mel, audio_mask)
+
+        fusion_weight = self.alpha_mlp(torch.cat(text, audio), dim = -1)
+        fusion_vector = fusion_weight * text + (1-fusion_weight) * audio
+        fusion_vector = fusion_vector.contiguous().view(b, h, self.hidden) # b, h, c
+
+        slf_attn_mask = history_mask.unsqueeze(1).expand(-1, h, -1)
+        output, attn = self.attention(fusion_vector, fusion_vector, fusion_vector, slf_attn_mask)
+        return output, attn
+
